86702af2865ac96298da3ed59cd81560e8c0fde1
Dainik Pandya <dainikpandya@gmail.com>
2017-08-04 00:42:26 +0530

    First Commit

---
+++ RedisPublisher.java
@@ -0,0 +1,21 @@
+import redis.clients.jedis.Jedis;
+
+import java.util.Scanner;
+
+ 
+public class RedisPublisher {
+    public static void main(String  args[]){
+        Jedis jedis = new Jedis("localhost");
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Enter the channel name:");
+        String channel=scanner.nextLine();
+        System.out.println("Starting publisher for channel "+ channel);
+
+        while (true){
+            System.out.println("Enter the string to Publish:");
+            String msg = scanner.nextLine();
+            jedis.publish(channel,msg);
+
+        }
+    }
+}
---
+++ RedisSubscriber.java
@@ -0,0 +1,50 @@
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPubSub;
+
+import java.util.Scanner;
+
+public class RedisSubscriber {
+
+    public static void main(String args[]) {
+        Jedis jedis = new Jedis("localhost");
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Enter the channel name:");
+        String channel = scanner.nextLine();
+        System.out.println("Starting subscriber for channel " + channel);
+
+        while (true) {
+            jedis.subscribe(new JedisPubSub() {
+                @Override
+                public void onMessage(String channel, String message) {
+                    super.onMessage(channel, message);
+                    System.out.println("Received message:" + message);
+                }
+
+                @Override
+                public void onSubscribe(String channel, int subscribedChannels) {
+                }
+
+                @Override
+                public void onUnsubscribe(String channel, int subscribedChannels) {
+                }
+
+                @Override
+                public void onPMessage(String pattern, String channel, String message) {
+                }
+
+                @Override
+                public void onPUnsubscribe(String pattern, int subscribedChannels) {
+                }
+                
+                @Override
+                public void onPSubscribe(String pattern, int subscribedChannels) {
+                }
+
+            }, channel);
+        }
+    }
+}
+
+
+
+
